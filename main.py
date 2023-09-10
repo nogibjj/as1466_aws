5fea6c62ad46ef2c5d6ec7a92cd2af59d2a46c1c
Nastiiasaenko <54864655+Nastiiasaenko@users.noreply.github.com>
2023-09-10 16:04:59 -0400

    add pandas function to py script

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-def basic_two_calculation(x,y): 
-  return x+y
+import pandas as pd
+df = pd.read_csv('bmi.csv')
+
+#function to populate all descriptive statistics
+def stats(df): 
+  return df.describe()
+#function  to return mean of a Series 
+def mean(series):
+  return series.mean()
+#function to return median of Series 
+def median(series): 
+  return series.median()
+#function to return standard deviation of statistics 
+def std(series):
+  return series.std()
