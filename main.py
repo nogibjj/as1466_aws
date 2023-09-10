7965575c8d299e951c37a73731822f8bd0258f2e
Nastiiasaenko <54864655+Nastiiasaenko@users.noreply.github.com>
2023-09-10 17:02:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,17 @@ def median(series):
 #function to return standard deviation of statistics 
 def std(series):
   return series.std()
+  
+#main script
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns 
+
+#reading csv file
+data = pd.read_csv('bmi.csv')
+
+#generate summary statistics 
+stats1 = stats(data)
+#generate boxplot 
+boxplots = sns.boxplot(df)
+plt.savefig('boxplots.png')
